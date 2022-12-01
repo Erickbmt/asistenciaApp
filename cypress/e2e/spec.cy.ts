31aeb61d8de472ee4e94bc062ad31b4abf56619d
Erickbmt <erickesohnomahlol@gmail.com>
2022-12-01 02:14:20 -0300

    e2e ?????
    Tengo un iq mayor que el promedio

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -8,29 +8,80 @@ describe('Verificar mi aplicacion', () =>{
       cy.contains('Ingresa a tu cuenta').click();
       cy.intercept('/home').as('route').then(() =>{
         cy.get('ion-title').should('contain.text', 'Sistema de Asistencia DUOC');
-        cy.get('#saludo').should('contain.text', '¡Bienvenido Nakito Tempest!')
+        // cy.get('#saludo').should('contain.text', '¡Bienvenido Nakito Tempest!')
       });
     });
   });
 
-    it('Verficar login con credenciales correctas', () => {
-      cy.wait(5000);
-      cy.visit('http://localhost:8100').then(() => {
+  it('Verficar login con credenciales correctas', () => {
+    cy.wait(5000);
+    cy.visit('http://localhost:8100').then(() => {
+      cy.wait(3000);
+      cy.get('#correo').invoke('val', 'jperez@duocuc.cl');
+      cy.wait(3000);
+      cy.get('#password').invoke('val', '5678');
+      cy.wait(3000);
+      cy.contains('Ingresa a tu cuenta').click();
+      cy.intercept('/home').as('route').then(() =>{
+        cy.wait(3000);
+        cy.get('ion-title').should('contain.text', 'Sistema de Asistencia DUOC');
+        // cy.get('#saludo').should('contain.text', '¡Bienvenido Juan Pérez González!')
+        cy.wait(3000);
+        cy.contains('Salir').click();
+      });
+    });
+  
+  it('Verificar olvidar contraseña correcto', () => {
+    cy.wait(5000);
+    cy.visit('http://localhost:8100').then(()=> {
+      cy.wait(3000);
+      cy.contains('¿Olvidaste tu contraseña?').click();
+      cy.intercept('/correo').as('route').then(() =>{
         cy.wait(3000);
         cy.get('#correo').invoke('val', 'jperez@duocuc.cl');
         cy.wait(3000);
-        cy.get('#password').invoke('val', '5678');
-        cy.wait(3000);
-        cy.contains('Ingresa a tu cuenta').click();
-        cy.intercept('/home').as('route').then(() =>{
+        cy.contains('Recuperar contraseña').click();
+        cy.intercept('/pregunta').as('route').then(() => {
           cy.wait(3000);
-          cy.get('ion-title').should('contain.text', 'Sistema de Asistencia DUOC');
-          cy.get('#saludo').should('contain.text', '¡Bienvenido Juan Pérez González!')
+          cy.get('#respuesta').invoke('val', 'panqueques');
           cy.wait(3000);
-          cy.contains('Salir').click();
-        });
-      });
+          cy.contains('Recuperar contraseña').click();
+          cy.intercept('/correcto').as('rorute').then(() => {
+            cy.wait(5000);
+            cy.contains('Iniciar sesion').click();
+          })
 
+        })
+      })
+    })
+  });
+
+  it('Verificar olvidar contraseña incorrecto', () => {
+    cy.wait(5000);
+    cy.visit('http://localhost:8100').then(()=> {
+      cy.wait(3000);
+      cy.contains('¿Olvidaste tu contraseña?').click();
+      cy.intercept('/correo').as('route').then(() =>{
+        cy.wait(3000);
+        cy.get('#correo').invoke('val', 'jperez@duocuc.cl');
+        cy.wait(3000);
+        cy.contains('Recuperar contraseña').click();
+        cy.intercept('/pregunta').as('route').then(() => {
+          cy.wait(3000);
+          cy.get('#respuesta').invoke('val', 'gato');
+          cy.wait(3000);
+          cy.contains('Recuperar contraseña').click();
+          cy.intercept('/incorrecto').as('rorute').then(() => {
+            cy.wait(5000);
+            cy.contains('Iniciar sesion').click();
+          })
+
+        })
+      })
+    })
+  });
+      
+  
       
   });
 });
